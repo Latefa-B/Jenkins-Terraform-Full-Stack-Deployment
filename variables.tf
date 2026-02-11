c4bdf8330e917f9fdbfa31c72a844b803f58e72c
Latefa-B <benzakourlatefa@gmail.com>
2026-02-11 04:03:34 +0000

    Update existing vpc names on variables.tf file

--- variables.tf
+++ variables.tf
@@ -13,6 +13,6 @@ variable "environment_tag" {
 variable "eks_cluster_name_for_vpc_lookup" {
   description = "Name of the EKS cluster to lookup its VPC (from Lab 14)"
   type        = string
-  default     = "my-k8s-cluster" # <<< REPLACE THIS if you changed EKS cluster name
+  default     = "aws-default" # <<< REPLACE THIS if you changed EKS cluster name
 }
 
