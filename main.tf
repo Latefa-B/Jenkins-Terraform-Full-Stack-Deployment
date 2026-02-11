4d768505266d8d7525f8cad9a80fefedea15e4de
Latefa-B <benzakourlatefa@gmail.com>
2026-02-11 03:57:47 +0000

    Trigger Terraform pipeline

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ resource "aws_s3_bucket_public_access_block" "my_jenkins_managed_bucket_public_a
   bucket = aws_s3_bucket.my_jenkins_managed_bucket.id
 
   block_public_acls       = true
-  block_public_dre_grants = true
+  block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
